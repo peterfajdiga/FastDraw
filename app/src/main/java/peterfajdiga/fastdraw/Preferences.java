06a88d1a049d38bd6eb39c4bf8597928e7d8b105
Peter Fajdiga <peter.fajdiga@gmail.com>
2017-07-07 01:16:04 +0200

    appItemResource preference

--- app/src/main/java/peterfajdiga/fastdraw/Preferences.java
+++ app/src/main/java/peterfajdiga/fastdraw/Preferences.java
@@ -10,8 +10,9 @@ public final class Preferences {
     public static final int ACTION_SHORTCUT  = 2;
     public static final int ACTION_SETTINGS  = 3;
 
+    private static int appItemResource;
     public static int appItemResource() {
-        return R.layout.app_item;
+        return appItemResource;
     }
 
     private static int mainLayoutResource;
@@ -62,6 +63,12 @@ public final class Preferences {
 
     public static void loadPreferences(final Context context) {
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        switch (prefs.getString("appItemResource", "0")) {
+            default:
+            case "0": appItemResource = R.layout.app_item; break;
+            case "1": appItemResource = R.layout.app_item_horizontal; break;
+            case "2": appItemResource = R.layout.app_item_package; break;
+        }
         mainLayoutResource = prefs.getBoolean("headerbtm", false) ? R.layout.activity_main_headerbtm : R.layout.activity_main_headertop;
         stackFromBottom = prefs.getBoolean("stackFromBottom", false);
         statusBarDarker = prefs.getBoolean("statusBarDarker", false);
