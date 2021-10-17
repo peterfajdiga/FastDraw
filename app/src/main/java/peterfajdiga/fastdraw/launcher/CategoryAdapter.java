658a1013367510f7929e2af77f5d6c586de1dbfa
Peter Fajdiga <peter.fajdiga@gmail.com>
2021-10-17 09:36:11 +0200

    CategoryAdapter: avoid calling findViewById in onBindViewHolder

--- app/src/main/java/peterfajdiga/fastdraw/launcher/CategoryAdapter.java
+++ app/src/main/java/peterfajdiga/fastdraw/launcher/CategoryAdapter.java
@@ -86,54 +86,7 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ItemVi
     @SuppressLint("ClickableViewAccessibility")
     @Override
     public void onBindViewHolder(@NonNull final ItemViewHolder holder, final int position) {
-        final DisplayItem item = items.get(position);
-
-        final ImageView appIcon = holder.view.findViewById(R.id.app_item_icon);
-        appIcon.setImageDrawable(item.icon);
-
-        final TextView appLabel = holder.view.findViewById(R.id.app_item_name);
-        appLabel.setText(item.label);
-
-        holder.view.setOnClickListener(view -> {
-            final ActivityOptions opts;
-            if (Build.VERSION.SDK_INT >= 23) {
-                opts = ActivityOptions.makeClipRevealAnimation(view, 0, 0, view.getWidth(), view.getHeight());
-            } else {
-                opts = ActivityOptions.makeScaleUpAnimation(view, 0, 0, view.getWidth(), view.getHeight());
-            }
-
-            final Context context = holder.view.getContext();
-
-            try {
-                item.launchable.launch(context, launchManager, opts.toBundle(), view.getClipBounds());
-            } catch (final Exception e) {
-                Log.e("OreoShortcutLaunchable", "Failed to launch shortcut " + item.id, e);
-                Toast.makeText(context, R.string.error_launch_exception, Toast.LENGTH_LONG).show();
-            }
-        });
-
-        final PointF touchPoint = new PointF();
-        holder.view.setOnTouchListener((view, event) -> {
-            touchPoint.set(event.getX(), event.getY());
-            return false;
-        });
-        holder.view.setOnLongClickListener(view -> {
-            view.performHapticFeedback(HapticFeedbackConstants.LONG_PRESS);
-
-            final float x = view.getX() + touchPoint.x;
-            final float y = view.getY() + touchPoint.y;
-
-            // start drag
-            final View.DragShadowBuilder shadow = new OffsetDragShadowBuilder(view, x, y);
-            if (Build.VERSION.SDK_INT < 24) {
-                view.startDrag(null, shadow, null, 0);
-            } else {
-                view.startDragAndDrop(null, shadow, null, 0);
-            }
-            itemDragListener.onDragStarted(view, item.source);
-
-            return false;
-        });
+        holder.bind(items.get(position), launchManager, itemDragListener);
     }
 
     @Override
@@ -142,11 +95,62 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ItemVi
     }
 
     static class ItemViewHolder extends RecyclerView.ViewHolder {
-        final View view;
+        private final View view;
+        private final TextView label;
+        private final ImageView icon;
 
-        public ItemViewHolder(@NonNull final View itemView) {
+        ItemViewHolder(@NonNull final View itemView) {
             super(itemView);
             this.view = itemView;
+            this.label = itemView.findViewById(R.id.app_item_name);
+            this.icon = itemView.findViewById(R.id.app_item_icon);
+        }
+
+        @SuppressLint("ClickableViewAccessibility")
+        void bind(final DisplayItem item, final LaunchManager launchManager, final Launcher.ItemDragListener itemDragListener) {
+            icon.setImageDrawable(item.icon);
+            label.setText(item.label);
+
+            view.setOnClickListener(view -> {
+                final ActivityOptions opts;
+                if (Build.VERSION.SDK_INT >= 23) {
+                    opts = ActivityOptions.makeClipRevealAnimation(view, 0, 0, view.getWidth(), view.getHeight());
+                } else {
+                    opts = ActivityOptions.makeScaleUpAnimation(view, 0, 0, view.getWidth(), view.getHeight());
+                }
+
+                final Context context = view.getContext();
+
+                try {
+                    item.launchable.launch(context, launchManager, opts.toBundle(), view.getClipBounds());
+                } catch (final Exception e) {
+                    Log.e("OreoShortcutLaunchable", "Failed to launch shortcut " + item.id, e);
+                    Toast.makeText(context, R.string.error_launch_exception, Toast.LENGTH_LONG).show();
+                }
+            });
+
+            final PointF touchPoint = new PointF();
+            view.setOnTouchListener((view, event) -> {
+                touchPoint.set(event.getX(), event.getY());
+                return false;
+            });
+            view.setOnLongClickListener(view -> {
+                view.performHapticFeedback(HapticFeedbackConstants.LONG_PRESS);
+
+                final float x = view.getX() + touchPoint.x;
+                final float y = view.getY() + touchPoint.y;
+
+                // start drag
+                final View.DragShadowBuilder shadow = new OffsetDragShadowBuilder(view, x, y);
+                if (Build.VERSION.SDK_INT < 24) {
+                    view.startDrag(null, shadow, null, 0);
+                } else {
+                    view.startDragAndDrop(null, shadow, null, 0);
+                }
+                itemDragListener.onDragStarted(view, item.source);
+
+                return false;
+            });
         }
     }
 }
