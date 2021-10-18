345e0045ffe738ef56648af9c86b6823a4854fba
Peter Fajdiga <peter.fajdiga@gmail.com>
2021-10-18 20:26:54 +0200

    Preferences: reorder fields

--- app/src/main/java/peterfajdiga/fastdraw/Preferences.java
+++ app/src/main/java/peterfajdiga/fastdraw/Preferences.java
@@ -5,21 +5,20 @@ import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.preference.PreferenceManager;
 
+import androidx.annotation.ColorInt;
+
 public final class Preferences {
     public static boolean allowOrientation;
     public static boolean appsLinearList;
+    public static int widgetHeight;
     public static boolean headerOnBottom;
     public static boolean scrollableTabs;
     public static boolean hideHidden;
 
     public static boolean showIcons;
     public static boolean largeSingle;
-
     public static boolean headerSeparator;
-
-    public static int headerBgColor;
-
-    public static int widgetHeight;
+    @ColorInt public static int headerBgColor;
 
     public static void loadPreferences(final Context context) {
         final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
@@ -31,6 +30,7 @@ public final class Preferences {
             case "1": appsLinearList = true; break;
         }
         allowOrientation                   = prefs.getBoolean("allowOrientation",    res.getBoolean(R.bool.default_allowOrientation));
+        widgetHeight                       = prefs.getInt("widgetHeight",            res.getInteger(R.integer.default_widgetHeight));
         headerOnBottom                     = prefs.getBoolean("headerbtm",           res.getBoolean(R.bool.default_headerbtm));
         scrollableTabs                     = prefs.getBoolean("scrollableTabs",      res.getBoolean(R.bool.default_scrollableTabs));
         hideHidden                         = prefs.getBoolean("hideHidden",          res.getBoolean(R.bool.default_hideHidden));
@@ -38,6 +38,5 @@ public final class Preferences {
         largeSingle                        = prefs.getBoolean("largeSingle",         res.getBoolean(R.bool.default_largeSingle));
         headerSeparator                    = prefs.getBoolean("headerSeparator",     res.getBoolean(R.bool.default_headerSeparator));
         headerBgColor                      = prefs.getInt("headerBgColor",           res.getInteger(R.integer.default_headerBgColor));
-        widgetHeight                       = prefs.getInt("widgetHeight",            res.getInteger(R.integer.default_widgetHeight));
     }
 }
