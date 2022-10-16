peterfajdiga/FastDraw

commit b6fbe432bac9869d2f9e04fdaa66ce7f5c1221a1

drag&drop: handle dragged view visual effects in CategoryAdapter code

app/src/main/java/peterfajdiga/fastdraw/activities/MainActivity.java
@@ -18,8 +18,6 @@ import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.content.res.Resources;
 import android.graphics.Color;
-import android.graphics.LightingColorFilter;
-import android.graphics.Paint;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.TransitionDrawable;
 import android.net.Uri;
@@ -223,14 +221,7 @@ public class MainActivity extends FragmentActivity implements
     @SuppressLint("ClickableViewAccessibility")
     private void setupAppsPager(final NestedScrollParent scrollParent, final View.OnTouchListener longPressListener) {
         final ViewPager appsPager = findViewById(R.id.apps_pager);
-        launcher = new Launcher(
-            launchManager,
-            (draggedView, draggedItem) -> {
-                this.draggedView = draggedView;
-            },
-            longPressListener,
-            appsPager
-        );
+        launcher = new Launcher(launchManager, longPressListener, appsPager);
 
         setupWallpaperParallax(appsPager);
         setupHeader(appsPager);
@@ -462,7 +453,6 @@ public class MainActivity extends FragmentActivity implements
                 }
                 case DragEvent.ACTION_DRAG_ENDED: {
                     resetDragPager();
-                    resetDragItem();
                     return true;
                 }
                 default: {
@@ -897,14 +887,7 @@ public class MainActivity extends FragmentActivity implements
         launcher.addItems(getString(R.string.default_shortcut_category), newShortcut);
     }
 
-    // LauncherItem dragging
-    private View draggedView = null;
-
     private void startDrag(final LauncherItem draggedItem) {
-        final Paint silhouettePaint = new Paint();
-        silhouettePaint.setColorFilter(new LightingColorFilter(Color.BLACK, Color.BLACK));
-        draggedView.setLayerType(View.LAYER_TYPE_SOFTWARE, silhouettePaint);
-
         // show drop zones
         findViewById(R.id.apps_pager).animate().alpha(0.2f);
         findViewById(R.id.widget_container).animate().alpha(0.2f);
@@ -937,13 +920,6 @@ public class MainActivity extends FragmentActivity implements
                 final TransitionDrawable backgroundTransition = (TransitionDrawable)background;
                 backgroundTransition.reverseTransition(DROPZONE_TRANSITION_DURATION);
             }
-        }
-    }
-
-    public void resetDragItem() {
-        if (draggedView != null) {
-            draggedView.setLayerType(View.LAYER_TYPE_NONE, null);
-            draggedView = null;
         }
     }
 

app/src/main/java/peterfajdiga/fastdraw/launcher/Category.java
@@ -26,11 +26,10 @@ public class Category {
 
     public Category(
         final Context context,
-        final Launcher.ItemDragListener itemDragListener,
         final View.OnTouchListener backgroundTouchListener,
         final LaunchManager launchManager
     ) {
-        this.adapter = new CategoryAdapter(itemDragListener, launchManager);
+        this.adapter = new CategoryAdapter(launchManager);
         view = createView(context, backgroundTouchListener, adapter, nestedScrollChildManager);
     }
 

app/src/main/java/peterfajdiga/fastdraw/launcher/CategoryAdapter.java
@@ -3,8 +3,12 @@ package peterfajdiga.fastdraw.launcher;
 import android.annotation.SuppressLint;
 import android.app.ActivityOptions;
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.LightingColorFilter;
+import android.graphics.Paint;
 import android.graphics.PointF;
 import android.util.Log;
+import android.view.DragEvent;
 import android.view.HapticFeedbackConstants;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -22,12 +26,10 @@ import peterfajdiga.fastdraw.R;
 import peterfajdiga.fastdraw.launcher.displayitem.DisplayItem;
 
 public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ItemViewHolder> {
-    private final Launcher.ItemDragListener itemDragListener;
     private final LaunchManager launchManager;
     private final SortedList<DisplayItem> items;
 
-    public CategoryAdapter(@NonNull final Launcher.ItemDragListener itemDragListener, @NonNull final LaunchManager launchManager) {
-        this.itemDragListener = itemDragListener;
+    public CategoryAdapter(@NonNull final LaunchManager launchManager) {
         this.launchManager = launchManager;
         this.items = new SortedList<>(DisplayItem.class, new SortedList.Callback<DisplayItem>() {
             @Override
@@ -85,7 +87,7 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ItemVi
     @SuppressLint("ClickableViewAccessibility")
     @Override
     public void onBindViewHolder(@NonNull final ItemViewHolder holder, final int position) {
-        holder.bind(items.get(position), launchManager, itemDragListener);
+        holder.bind(items.get(position), launchManager);
     }
 
     @Override
@@ -106,7 +108,7 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ItemVi
         }
 
         @SuppressLint("ClickableViewAccessibility")
-        void bind(final DisplayItem item, final LaunchManager launchManager, final Launcher.ItemDragListener itemDragListener) {
+        void bind(final DisplayItem item, final LaunchManager launchManager) {
             icon.setImageDrawable(item.icon);
             label.setText(item.label);
 
@@ -138,9 +140,27 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ItemVi
                 // start drag
                 final View.DragShadowBuilder shadow = new OffsetDragShadowBuilder(view, x, y);
                 view.startDragAndDrop(null, shadow, item.source, 0);
-                itemDragListener.setDragItem(view, item.source);
+
+                final Paint silhouettePaint = new Paint();
+                silhouettePaint.setColorFilter(new LightingColorFilter(Color.BLACK, Color.BLACK));
+                view.setLayerType(View.LAYER_TYPE_SOFTWARE, silhouettePaint);
 
                 return false;
+            });
+
+            view.setOnDragListener((view, event) -> {
+                switch (event.getAction()) {
+                    case DragEvent.ACTION_DRAG_STARTED: {
+                        return event.getLocalState() == item.source;
+                    }
+                    case DragEvent.ACTION_DRAG_ENDED: {
+                        view.setLayerType(View.LAYER_TYPE_NONE, null);
+                        return true;
+                    }
+                    default: {
+                        return false;
+                    }
+                }
             });
         }
     }

app/src/main/java/peterfajdiga/fastdraw/launcher/Launcher.java
@@ -32,14 +32,12 @@ public class Launcher {
     private final PrefMap itemCategoryMap;
     private final PrefMap categoriesOrderMap;
     private final LaunchManager launchManager;
-    private final ItemDragListener itemDragListener;
     private final View.OnTouchListener backgroundTouchListener;
     private final ViewPager pager;
     private final LauncherPagerAdapter adapter;
 
     public Launcher(
         @NonNull final LaunchManager launchManager,
-        @NonNull final ItemDragListener itemDragListener,
         @NonNull final View.OnTouchListener backgroundTouchListener,
         @NonNull final ViewPager pager
     ) {
@@ -47,7 +45,6 @@ public class Launcher {
         this.itemCategoryMap = new PrefMap(context, "categories"); // TODO: pass from outside
         this.categoriesOrderMap = new PrefMap(context, "categoryorder"); // TODO: pass from outside
         this.launchManager = launchManager;
-        this.itemDragListener = itemDragListener;
         this.backgroundTouchListener = backgroundTouchListener;
         this.pager = pager;
         this.adapter = new LauncherPagerAdapter(context);
@@ -162,7 +159,7 @@ public class Launcher {
         Category category = adapter.categories.get(categoryName);
         if (category == null) {
             final Context context = pager.getContext();
-            category = new Category(context, itemDragListener, backgroundTouchListener, launchManager);
+            category = new Category(context, backgroundTouchListener, launchManager);
             categoriesOrderMap.getIntCreate(categoryName, CategoryComparator.UNORDERED); // make sure the new category is added to the categoryorder prefs
             adapter.categories.put(categoryName, category);
             adapter.notifyDataSetChanged();
@@ -214,7 +211,7 @@ public class Launcher {
             if (adapter.categories.containsKey(categoryName)) {
                 continue;
             }
-            final Category category = new Category(pager.getContext(), itemDragListener, backgroundTouchListener, launchManager);
+            final Category category = new Category(pager.getContext(), backgroundTouchListener, launchManager);
             adapter.categories.put(categoryName, category);
         }
         adapter.notifyDataSetChanged();
@@ -293,9 +290,5 @@ public class Launcher {
 
     private void removeItemCategory(@NonNull final LauncherItem item) {
         itemCategoryMap.remove(item.getId());
-    }
-
-    public interface ItemDragListener {
-        void setDragItem(@NonNull View draggedView, @NonNull LauncherItem draggedItem);
     }
 }

commit ebf248c4af167624cf5333dbca7f89b89b9734e2

end drag immediately on ACTION_DROP even if the launcher item was dropped on itself (bug introduced in b6fbe432)

app/src/main/java/peterfajdiga/fastdraw/activities/MainActivity.java
@@ -64,6 +64,7 @@ import peterfajdiga.fastdraw.WallpaperColorUtils;
 import peterfajdiga.fastdraw.dialogs.ActionsSheet;
 import peterfajdiga.fastdraw.dialogs.NewCategoryDialog;
 import peterfajdiga.fastdraw.dialogs.RenameCategoryDialog;
+import peterfajdiga.fastdraw.dragdrop.DragEndQueue;
 import peterfajdiga.fastdraw.dragdrop.DropZone;
 import peterfajdiga.fastdraw.launcher.AppItemManager;
 import peterfajdiga.fastdraw.launcher.LaunchManager;
@@ -102,6 +103,7 @@ public class MainActivity extends FragmentActivity {
 
     private static WeakReference<MainActivity> instance;
     private final LaunchManager launchManager = new LaunchManager(this);
+    private final DragEndQueue dragEndQueue = new DragEndQueue();
     private Launcher launcher;
     private WidgetManager widgetManager;
     private final RenameCategoryDialog.Listener renameCategoryDialogListener = (oldCategoryName, newCategoryName) -> {
@@ -235,7 +237,7 @@ public class MainActivity extends FragmentActivity {
     @SuppressLint("ClickableViewAccessibility")
     private void setupAppsPager(final NestedScrollParent scrollParent, final View.OnTouchListener longPressListener) {
         final ViewPager appsPager = findViewById(R.id.apps_pager);
-        launcher = new Launcher(launchManager, longPressListener, appsPager);
+        launcher = new Launcher(launchManager, dragEndQueue, longPressListener, appsPager);
 
         setupWallpaperParallax(appsPager);
         setupHeader(appsPager);
@@ -459,6 +461,7 @@ public class MainActivity extends FragmentActivity {
                 }
                 case DragEvent.ACTION_DRAG_ENDED: { // fires always, but only after the drag shadow animation (the dragged item returning to its position) finishes
                     endDrag(); // end drag definitely
+                    dragEndQueue.onDragEnd();
                     return true;
                 }
                 default: {

app/src/main/java/peterfajdiga/fastdraw/dragdrop/DragEndQueue.java
@@ -1,0 +1,23 @@
+package peterfajdiga.fastdraw.dragdrop;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+import peterfajdiga.fastdraw.launcher.CategoryAdapter;
+
+public class DragEndQueue implements CategoryAdapter.DragEndService {
+    private final Queue<Runnable> queue = new LinkedList<>();
+
+    @Override
+    public void post(final Runnable f) {
+        queue.add(f);
+    }
+
+    public void onDragEnd() {
+        while (!queue.isEmpty()) {
+            final Runnable f = queue.poll();
+            assert f != null; // because queue was not empty
+            f.run();
+        }
+    }
+}

app/src/main/java/peterfajdiga/fastdraw/launcher/Category.java
@@ -26,10 +26,11 @@ public class Category {
 
     public Category(
         final Context context,
-        final View.OnTouchListener backgroundTouchListener,
-        final LaunchManager launchManager
+        final LaunchManager launchManager,
+        final CategoryAdapter.DragEndService dragEndService,
+        final View.OnTouchListener backgroundTouchListener
     ) {
-        this.adapter = new CategoryAdapter(launchManager);
+        this.adapter = new CategoryAdapter(launchManager, dragEndService);
         view = createView(context, backgroundTouchListener, adapter, nestedScrollChildManager);
     }
 

app/src/main/java/peterfajdiga/fastdraw/launcher/CategoryAdapter.java
@@ -8,7 +8,6 @@ import android.graphics.LightingColorFilter;
 import android.graphics.Paint;
 import android.graphics.PointF;
 import android.util.Log;
-import android.view.DragEvent;
 import android.view.HapticFeedbackConstants;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -27,10 +26,12 @@ import peterfajdiga.fastdraw.launcher.displayitem.DisplayItem;
 
 public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ItemViewHolder> {
     private final LaunchManager launchManager;
+    private final DragEndService dragEndService;
     private final SortedList<DisplayItem> items;
 
-    public CategoryAdapter(@NonNull final LaunchManager launchManager) {
+    public CategoryAdapter(@NonNull final LaunchManager launchManager, final DragEndService dragEndService) {
         this.launchManager = launchManager;
+        this.dragEndService = dragEndService;
         this.items = new SortedList<>(DisplayItem.class, new SortedList.Callback<DisplayItem>() {
             @Override
             public int compare(final DisplayItem o1, final DisplayItem o2) {
@@ -87,7 +88,7 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ItemVi
     @SuppressLint("ClickableViewAccessibility")
     @Override
     public void onBindViewHolder(@NonNull final ItemViewHolder holder, final int position) {
-        holder.bind(items.get(position), launchManager);
+        holder.bind(items.get(position), launchManager, dragEndService);
     }
 
     @Override
@@ -108,7 +109,7 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ItemVi
         }
 
         @SuppressLint("ClickableViewAccessibility")
-        void bind(final DisplayItem item, final LaunchManager launchManager) {
+        void bind(final DisplayItem item, final LaunchManager launchManager, final DragEndService dragEndService) {
             icon.setImageDrawable(item.icon);
             label.setText(item.label);
 
@@ -145,23 +146,14 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ItemVi
                     view.setLayerType(View.LAYER_TYPE_SOFTWARE, silhouettePaint);
                 }
 
+                dragEndService.post(()-> view.setLayerType(View.LAYER_TYPE_NONE, null)); // avoid creating an OnDragListener to prevent stealing ACTION_DROP from android.R.id.content
+
                 return false;
             });
-
-            view.setOnDragListener((view, event) -> {
-                switch (event.getAction()) {
-                    case DragEvent.ACTION_DRAG_STARTED: {
-                        return event.getLocalState() == item.source;
-                    }
-                    case DragEvent.ACTION_DRAG_ENDED: {
-                        view.setLayerType(View.LAYER_TYPE_NONE, null);
-                        return true;
-                    }
-                    default: {
-                        return false;
-                    }
-                }
-            });
         }
+    }
+
+    public interface DragEndService {
+        void post(Runnable f);
     }
 }

app/src/main/java/peterfajdiga/fastdraw/launcher/Launcher.java
@@ -32,12 +32,14 @@ public class Launcher {
     private final PrefMap itemCategoryMap;
     private final PrefMap categoriesOrderMap;
     private final LaunchManager launchManager;
+    private final CategoryAdapter.DragEndService dragEndService;
     private final View.OnTouchListener backgroundTouchListener;
     private final ViewPager pager;
     private final LauncherPagerAdapter adapter;
 
     public Launcher(
         @NonNull final LaunchManager launchManager,
+        @NonNull final CategoryAdapter.DragEndService dragEndService,
         @NonNull final View.OnTouchListener backgroundTouchListener,
         @NonNull final ViewPager pager
     ) {
@@ -45,6 +47,7 @@ public class Launcher {
         this.itemCategoryMap = new PrefMap(context, "categories"); // TODO: pass from outside
         this.categoriesOrderMap = new PrefMap(context, "categoryorder"); // TODO: pass from outside
         this.launchManager = launchManager;
+        this.dragEndService = dragEndService;
         this.backgroundTouchListener = backgroundTouchListener;
         this.pager = pager;
         this.adapter = new LauncherPagerAdapter(context);
@@ -159,7 +162,7 @@ public class Launcher {
         Category category = adapter.categories.get(categoryName);
         if (category == null) {
             final Context context = pager.getContext();
-            category = new Category(context, backgroundTouchListener, launchManager);
+            category = new Category(context, launchManager, dragEndService, backgroundTouchListener);
             categoriesOrderMap.getIntCreate(categoryName, CategoryComparator.UNORDERED); // make sure the new category is added to the categoryorder prefs
             adapter.categories.put(categoryName, category);
             adapter.notifyDataSetChanged();
@@ -211,7 +214,7 @@ public class Launcher {
             if (adapter.categories.containsKey(categoryName)) {
                 continue;
             }
-            final Category category = new Category(pager.getContext(), backgroundTouchListener, launchManager);
+            final Category category = new Category(pager.getContext(), launchManager, dragEndService, backgroundTouchListener);
             adapter.categories.put(categoryName, category);
         }
         adapter.notifyDataSetChanged();