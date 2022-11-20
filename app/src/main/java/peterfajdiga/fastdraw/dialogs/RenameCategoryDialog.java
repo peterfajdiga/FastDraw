76a7197e4a799867f640a1e7ca995d8a1f89bbc6
Peter Fajdiga <peter.fajdiga@gmail.com>
2022-11-20 15:31:35 +0100

    handle INITIAL_CATEGORY_NAME_KEY in MainActivity

--- app/src/main/java/peterfajdiga/fastdraw/dialogs/RenameCategoryDialog.java
+++ app/src/main/java/peterfajdiga/fastdraw/dialogs/RenameCategoryDialog.java
@@ -5,30 +5,23 @@ import android.app.Activity;
 import androidx.annotation.NonNull;
 
 public class RenameCategoryDialog extends CategorySelectionDialog {
-    private static final String INITIAL_CATEGORY_NAME_KEY = "categoryName";
-
     @Override
     public void onCategorySelected(@NonNull final String categoryName) {
         final Activity activity = requireActivity();
-        if (activity instanceof RenameCategoryDialog.Listener) {
-            ((RenameCategoryDialog.Listener)activity).onRenameCategoryDialogSuccess(
-                this,
-                this.requireArguments().getString(INITIAL_CATEGORY_NAME_KEY),
-                categoryName
-            );
+        if (activity instanceof Listener) {
+            ((Listener)activity).onRenameCategoryDialogSuccess(this, categoryName);
         } else {
             throw new RuntimeException(activity.getLocalClassName() + " must implement RenameCategoryDialog.Listener");
         }
     }
 
     public interface Listener {
-        void onRenameCategoryDialogSuccess(CategorySelectionDialog dialog, String oldCategoryName, String newCategoryName);
+        void onRenameCategoryDialogSuccess(CategorySelectionDialog dialog, String categoryName);
     }
 
-    public static RenameCategoryDialog newInstance(@NonNull final String initialCategoryName, @NonNull final String title) {
+    public static RenameCategoryDialog newInstance(@NonNull final String title) {
         final RenameCategoryDialog dialog = new RenameCategoryDialog();
         dialog.setup(title);
-        DialogUtils.modifyArguments(dialog, args -> args.putString(INITIAL_CATEGORY_NAME_KEY, initialCategoryName));
         return dialog;
     }
 }
