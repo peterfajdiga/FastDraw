8fc370397164c71819a36e44b5d0942881364c98
Peter Fajdiga <peter.fajdiga@gmail.com>
2023-04-15 12:30:40 +0200

    InstallShortcutReceiver: remove outdated comment

--- app/src/main/java/peterfajdiga/fastdraw/receivers/InstallShortcutReceiver.java
+++ app/src/main/java/peterfajdiga/fastdraw/receivers/InstallShortcutReceiver.java
@@ -14,9 +14,6 @@ public class InstallShortcutReceiver extends BroadcastReceiver {
         final FiledShortcutItem newShortcutItem = ShortcutItemManager.shortcutFromIntent(context, data);
         ShortcutItemManager.saveShortcut(context, newShortcutItem);
 
-        // TODO: Find out if it's necessary to save SHORTCUT category. Now it'll probably show up in LOST&FOUND if MainActivity == null
-        // final String category = context.getString(R.string.default_shortcut_category);
-
         final MainActivity activity = MainActivity.getInstance();
         if (activity != null) {
             activity.onShortcutReceived(newShortcutItem);
