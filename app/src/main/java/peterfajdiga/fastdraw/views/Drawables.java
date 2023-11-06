43df933118ed007a63f0dfb29d2b0a85844c429b
Peter Fajdiga <peter.fajdiga@gmail.com>
2023-11-06 21:31:59 +0100

    Drawables: extract helper function `createBorderedDrawable`

--- app/src/main/java/peterfajdiga/fastdraw/views/Drawables.java
+++ app/src/main/java/peterfajdiga/fastdraw/views/Drawables.java
@@ -34,63 +34,6 @@ public class Drawables {
         return transitionDrawable;
     }
 
-    public static Drawable createHeaderBackgroundCollapsed(
-        @NonNull final Resources res,
-        @ColorInt final int color,
-        final boolean bgGradient,
-        final boolean divider,
-        final boolean dividerBottom
-    ) {
-        final Drawable gradientDrawable;
-        if (bgGradient) {
-            gradientDrawable = new GradientDrawable(GradientDrawable.Orientation.TOP_BOTTOM, new int[]{
-                Color.TRANSPARENT,
-                color,
-            });
-        } else {
-            gradientDrawable = new ColorDrawable(Color.TRANSPARENT);
-        }
-
-        if (divider) {
-            final LayerDrawable layers = new LayerDrawable(new Drawable[]{
-                gradientDrawable,
-                new ColorDrawable(res.getColor(R.color.headerDividerColor)),
-            });
-            final int dividerHeight = Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 1.0f, res.getDisplayMetrics()));
-            layers.setLayerHeight(1, dividerHeight);
-            if (dividerBottom) {
-                layers.setLayerGravity(1, Gravity.BOTTOM);
-            } else {
-                layers.setLayerGravity(1, Gravity.TOP);
-            }
-            return layers;
-        } else {
-            return gradientDrawable;
-        }
-    }
-
-    public static Drawable createHeaderBackgroundExpanded(
-        @NonNull final Resources res,
-        @ColorInt final int color,
-        final boolean dividers
-    ) {
-        if (dividers) {
-            final LayerDrawable layers = new LayerDrawable(new Drawable[]{
-                new ColorDrawable(color),
-                new ColorDrawable(res.getColor(R.color.headerDividerColor)),
-                new ColorDrawable(res.getColor(R.color.headerDividerColor)),
-            });
-            final int dividerHeight = Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 1.0f, res.getDisplayMetrics()));
-            layers.setLayerHeight(1, dividerHeight);
-            layers.setLayerGravity(1, Gravity.TOP);
-            layers.setLayerHeight(2, dividerHeight);
-            layers.setLayerGravity(2, Gravity.BOTTOM);
-            return layers;
-        } else {
-            return new ColorDrawable(color);
-        }
-    }
-
     public static Drawable createBgGradientDrawable(
         @ColorInt final int color,
         final int heightTop,
@@ -118,5 +61,80 @@ public class Drawables {
         }
 
         return layers;
+    }
+
+    private static Drawable createHeaderBackgroundCollapsed(
+        @NonNull final Resources res,
+        @ColorInt final int color,
+        final boolean bgGradient,
+        final boolean divider,
+        final boolean dividerBottom
+    ) {
+        final Drawable gradientDrawable;
+        if (bgGradient) {
+            gradientDrawable = new GradientDrawable(GradientDrawable.Orientation.TOP_BOTTOM, new int[]{
+                Color.TRANSPARENT,
+                color,
+            });
+        } else {
+            gradientDrawable = new ColorDrawable(Color.TRANSPARENT);
+        }
+
+        return createBorderedDrawable(
+            res,
+            gradientDrawable,
+            divider && !dividerBottom,
+            divider && dividerBottom
+        );
+    }
+
+    private static Drawable createHeaderBackgroundExpanded(
+        @NonNull final Resources res,
+        @ColorInt final int color,
+        final boolean dividers
+    ) {
+        return createBorderedDrawable(
+            res,
+            new ColorDrawable(color),
+            dividers,
+            dividers
+        );
+    }
+
+    private static Drawable createBorderedDrawable(
+        @NonNull final Resources res,
+        final Drawable content,
+        final boolean topBorder,
+        final boolean bottomBorder
+    ) {
+        final int borderCount = (topBorder ? 1 : 0) + (bottomBorder ? 1 : 0);
+        if (borderCount == 0) {
+            return content;
+        }
+
+        @ColorInt final int borderColor = res.getColor(R.color.headerDividerColor);
+        final int borderThickness = Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 1.0f, res.getDisplayMetrics()));
+
+        final Drawable[] layers = new Drawable[borderCount+1];
+        layers[0] = content;
+        final int topBorderIndex = 1;
+        final int bottomBorderIndex = borderCount;
+        if (topBorder) {
+            layers[topBorderIndex] = new ColorDrawable(borderColor);
+        }
+        if (bottomBorder) {
+            layers[bottomBorderIndex] = new ColorDrawable(borderColor);
+        }
+
+        final LayerDrawable layerDrawable = new LayerDrawable(layers);
+        if (topBorder) {
+            layerDrawable.setLayerHeight(topBorderIndex, borderThickness);
+            layerDrawable.setLayerGravity(topBorderIndex, Gravity.TOP);
+        }
+        if (bottomBorder) {
+            layerDrawable.setLayerHeight(bottomBorderIndex, borderThickness);
+            layerDrawable.setLayerGravity(bottomBorderIndex, Gravity.BOTTOM);
+        }
+        return layerDrawable;
     }
 }
