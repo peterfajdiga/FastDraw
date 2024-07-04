b2ea66a5fb650ed97667c11f051b1d4352d1b0b6
LJ <LJ@localhost>
2024-07-04 20:06:51 +0300

    Remove min widget height

--- app/src/main/java/peterfajdiga/fastdraw/activities/MainActivity.java
+++ app/src/main/java/peterfajdiga/fastdraw/activities/MainActivity.java
@@ -783,7 +783,7 @@ public class MainActivity extends FragmentActivity implements CategorySelectionD
         final float availableHeight = res.getDisplayMetrics().heightPixels - res.getDimension(R.dimen.pager_header_height);
         return Utils.clamp(
             configuredHeight + delta,
-            availableHeight * 0.2f,
+            1,
             availableHeight * 0.7f // TODO: handle landscape orientation
         );
     }
