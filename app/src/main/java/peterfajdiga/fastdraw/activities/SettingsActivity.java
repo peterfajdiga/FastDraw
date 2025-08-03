3da3674904436571e32be011bd2efc17f902419e
Peter Fajdiga <peter.fajdiga@gmail.com>
2025-08-03 16:29:45 +0200

    SettingsActivity: fix actionbar obscuring content

--- app/src/main/java/peterfajdiga/fastdraw/activities/SettingsActivity.java
+++ app/src/main/java/peterfajdiga/fastdraw/activities/SettingsActivity.java
@@ -21,6 +21,9 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.appcompat.app.ActionBar;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -111,6 +114,11 @@ public class SettingsActivity extends AppCompatPreferenceActivity implements Sha
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ViewCompat.setOnApplyWindowInsetsListener(getWindow().getDecorView(), (v, insets) -> {
+            final Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return WindowInsetsCompat.CONSUMED;
+        });
         setupActionBar();
     }
 
